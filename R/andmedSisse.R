2e0f109c0314331fd79c4579c88ee46f87e9a290
Risto Hinno <ristohinno@gmail.com>
2016-01-26 21:13:43 +0200

    description täiendus

--- R/andmedSisse.R
+++ R/andmedSisse.R
@@ -2,7 +2,7 @@
 andmedSisse=function(url) {
   #loeme andmed sisse
   library(jsonlite)
-  #library(data.table)
+  library(data.table)
   andmed=fromJSON(readLines(url), flatten=T)
   andmed=andmed["teenuste_kanalid_ja_moodikud"!="list()"]
   andmedMoodik <- rbindlist(lapply(andmed[["teenuste_kanalid_ja_moodikud"]], function(x) {
