358a469e6d783d115241143a38e5d1be668719d1
Risto Hinno <ristohinno@gmail.com>
2016-01-28 17:56:20 +0200

    lihtsustasin andmedSisse funktsiooni

--- R/andmedSisse.R
+++ R/andmedSisse.R
@@ -3,12 +3,12 @@
 andmedSisse=function(url) {
   #loeme andmed sisse
   library(jsonlite)
-  library(data.table)
+  #library(data.table)
   andmed=fromJSON(readLines(url), flatten=T)
-  andmed=andmed["teenuste_kanalid_ja_moodikud"!="list()"]
-  andmedMoodik <- rbindlist(lapply(andmed[["teenuste_kanalid_ja_moodikud"]], function(x) {
-    as.list(unlist(x))
-  }), fill=TRUE)
+  #andmed=andmed["teenuste_kanalid_ja_moodikud"!="list()"]
+  ##andmedMoodik <- rbindlist(lapply(andmed[["teenuste_kanalid_ja_moodikud"]], function(x) {
+   # as.list(unlist(x))
+  #}), fill=TRUE)
   andmed
 }
 
