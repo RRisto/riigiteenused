0af68d41d93d38625ddcd35fa05fe203c23274e4
Risto Hinno <ristohinno@gmail.com>
2016-08-29 20:10:21 +0300

    bugi parandus

--- R/andmedSisse.R
+++ R/andmedSisse.R
@@ -1,10 +1,17 @@
 #' andmete downloadi abifunktsioon
 #' @export
 andmedSisse=function(url="https://www.riigiteenused.ee/api/et/all") {
-  library(rjson)
+  #library(rjson)
   library(jsonlite)
   #loeme andmed sisse
-  fromJSON(url, flatten=T)
+  #fromJSON(url, flatten=T) ei funka
+  lai=fromJSON(url, flatten=T)
+  lai=lai[lai$teenuste_kanalid_ja_moodikud!="list()",]#kus on tühi sealt eemaldab
+  moodik <- rbindlist(lapply(lai[["teenuste_kanalid_ja_moodikud"]], function(x) {
+    as.list(unlist(x))
+  }), fill=TRUE)
+  lai$teenuste_kanalid_ja_moodikud=NULL
+  lai=cbind(lai,moodik)
 }
 
 
