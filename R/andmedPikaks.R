f8a385a82fbead3a5887e202fbef6d11f6e61a2b
Risto Hinno <ristohinno@gmail.com>
2017-01-06 09:45:12 +0200

    API versioon 2

--- R/andmedPikaks.R
+++ R/andmedPikaks.R
@@ -1,30 +1,47 @@
-#andmed pikaks, võtab eelenvad funktsioonid kokku, iga
-#aasta andmed teeb pikaks, kasutab data.table'i formaati
+#andmed pikaks, võtab eelnevad funktsioonid kokku, iga
+#aasta andmed teeb pikaks
 #' @export
 andmedPikaks=function(andmedLai) {
-  andmed=data.table(andmedLai)
-  #mõõtmiste aastate väärtused
-  aastad=c("2011", "2012","2013","2014","2015", "empty")
-  #col nimede list, kus iga mõõtmise aasta kohta on vajalikud andmed
-  colNimed=list()
-  for(i in 1:length(aastad)) {
-    colNimed[[i]]=c(grep(paste(paste0(aastad[-i],"."), collapse = "|"),
-                                   names(andmed), value=T, invert=T))
+  tulem=data.frame(NULL)
+  for (i in 1:nrow(andmedLai)) {
+    sub=andmedLai$serviceStatistics[i]
+    aastad=sub[[1]][2]
+    teenustulem=data.frame(NULL)
+    for (j in 1:nrow(aastad)) {
+      abikas=sub[[1]][3]$availableChannel[[j]]
+      moodikud=abikas$channelStatistics
+      taisrida=cbind(moodikud, abikas[, c("url", "additionalType", "payment")])
+      taisrida=cbind(taisrida, andmedLai[i, c("domain", "subdomain", "serviceType",
+                                              "provider.name", "provider.memberOf.name")])
+      taisrida$aasta=aastad$value[j]
+      teenustulem=rbind(teenustulem, taisrida)
+    }
+    teenustulem$id=andmedLai$identifier[i]
+    tulem=rbind(tulem, teenustulem)
   }
-  #mapply eri aastate andmed listi
-  andmedLaiList=mapply(function(colNimed, andmed)
-    andmed[, colNimed, names(andmed), with=F],colNimed,
-    MoreArgs=list(andmed=andmed))
-  names(andmedLaiList)=aastad#lisit elementidele nimed
-  #lisa mõõtmise aasta muutuja
-  andmedLaiList=Map(cbind, andmedLaiList, year = as.list(aastad))
-  #lapply läbi korrastaja
-  andmedLaiList <- lapply(andmedLaiList,
-                          function(df) {
-                            korrastaja(andmed=df, mootmiseAasta=df$year[1])
-                          })
-  #tee üheks data frame/table-ks
-  andmedPikk=rbindlist(andmedLaiList, fill=TRUE)
-  andmedPikk[, value:=as.numeric(as.character(value))]#tee characteriks
-  andmedPikk[, MootmiseAasta:=gsub("empty", "pole moodetud", MootmiseAasta)]
+
+  library(reshape2)
+  tulem$'@type'=NULL
+  #nimetame veerud ümber
+  names(tulem)[names(tulem)=="aasta"] <- "MootmiseAasta"
+  names(tulem)[names(tulem)=="payment"] <- "makse"
+  names(tulem)[names(tulem)=="id"] <- "identifikaator"
+  names(tulem)[names(tulem)=="domain"] <- "valdkond"
+  names(tulem)[names(tulem)=="subdomain"] <- "alamvaldkond"
+  names(tulem)[names(tulem)=="serviceType"] <- "teenusetyyp"
+  names(tulem)[names(tulem)=="provider.memberOf.name"] <- "ministeerium"
+  names(tulem)[names(tulem)=="provider.name"] <- "allasutus"
+  names(tulem)[names(tulem)=="additionalType"] <- "kanal"
+  names(tulem)[names(tulem)=="transaction"] <- "osutamistearv"
+  names(tulem)[names(tulem)=="satisfaction"] <- "rahulolu"
+  names(tulem)[names(tulem)=="cost"] <- "halduskulu"
+  names(tulem)[names(tulem)=="activeTimeSpent"] <- "ajakulu"
+  names(tulem)[names(tulem)=="totalTimeSpent"] <- "ajakuluBruto"
+
+  pikk=melt(tulem, measure.vars=c("osutamistearv", "rahulolu", "halduskulu",
+                                  "ajakulu", "ajakuluBruto"))
+  names(pikk)[names(pikk)=="variable"] <- "naitaja"
+  #kus on müüdik puudu paneme NA
+  pikk$value=ifelse(pikk$value=="", NA, pikk$value)
+  pikk
 }
--- R/andmedSisse.R
+++ R/andmedSisse.R
@@ -4,15 +4,8 @@ andmedSisse=function(url="https://www.riigiteenused.ee/api/et/all") {
   #library(rjson)
   library(jsonlite)
   #loeme andmed sisse
-  #fromJSON(url, flatten=T) ei funka
-  lai=fromJSON(url, flatten=T)
-  # lai=lai["teenuste_kanalid_ja_moodikud"!="list()",]#kus on tühi sealt eemaldab
-  # moodik <- rbindlist(lapply(lai[["teenuste_kanalid_ja_moodikud"]], function(x) {
-  #   as.list(unlist(x))
-  # }), fill=TRUE)
-  # lai$teenuste_kanalid_ja_moodikud=NULL
-  # lai=cbind(lai,moodik)
-  lai
+  lai=fromJSON(url)
+  flatten(lai[[2]])
 }
 
 
